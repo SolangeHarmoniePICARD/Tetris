c5fbabaef700ccd51dfc15eac1022e326f86a96e
Solange Harmonie PICARD <solange.picard@cryptolab.net>
2019-08-09 12:25:43 +0200

    Déplacements et rotation de la pièce au clavier.

--- tetris.js
+++ tetris.js
@@ -132,6 +132,26 @@ Piece.prototype.rotate = function () {
   this.draw();
 }
 
+//contrôler la pièce
+//On écoute un évènement, le fait d'appuier sur une touche.
+document.addEventListener("keydown", CONTROL);
+
+function CONTROL(event) {
+    if(event.keyCode == 37){
+        p.moveLeft();
+        dropStart = Date.now();
+    }else if(event.keyCode == 38){
+        p.rotate();
+        dropStart = Date.now();
+    }else if(event.keyCode == 39){
+        p.moveRight();
+        dropStart = Date.now();
+    }else if(event.keyCode == 40){
+        p.moveDown();
+    }
+}
+
+
 //la fréquence à laquelle les pièces vont tomber
 let dropStart = Date.now();
 function drop() {
