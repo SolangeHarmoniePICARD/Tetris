7eebb1b3558503cc7e573950d95c2c0c86969640
Solange Harmonie PICARD <solange.picard@cryptolab.net>
2019-08-09 17:37:51 +0200

    Les pièces se déplacent et tournent.

--- tetris.js
+++ tetris.js
@@ -48,8 +48,15 @@ const PIECES = [
   [J, "orange"]
 ];
 
+//creation de la fonctin random pour générer de nouvelles pièces
+function randomPiece() {
+  let r = randomN = Math.floor(Math.random() * PIECES.length);
+  return new Piece(PIECES[r][0], PIECES[r][1]);
+
+}
+
 //initier une pièce
-let p = new Piece(PIECES[0][0], PIECES[0][1]);
+let p = randomPiece();
 
 //l'objet pièce
 function Piece(tetromino, color) {
@@ -105,32 +112,143 @@ Piece.prototype.unDraw = function () {
 
 //faire tomber la pièce
 Piece.prototype.moveDown = function () {
-  this.unDraw();
-  this.y++;
-  this.draw();
+  if (!this.collision(0, 1, this.activeTetromino)) {
+    this.unDraw();
+    this.y++;
+    this.draw();
+  }else {
+    this.lock();
+    p = randomPiece();
+  }
+
 }
 
 //déplacer la pièce à droite
 Piece.prototype.moveRight = function () {
-  this.unDraw();
-  this.x++;
-  this.draw();
+  if (!this.collision(1, 0, this.activeTetromino)) {
+    this.unDraw();
+    this.x++;
+    this.draw();
+  }
+
 }
 
 //déplacer la pièce à gauche
 Piece.prototype.moveLeft = function () {
-  this.unDraw();
-  this.x--;
-  this.draw();
+  if (!this.collision(-1, 0, this.activeTetromino)) {
+    this.unDraw();
+    this.x--;
+    this.draw();
+  }
+
 }
 
 //effectuer une rotation
 Piece.prototype.rotate = function () {
-  this.unDraw();
-  this.tetrominoN = (this.tetrominoN + 1)%this.tetromino.length;
-  this.activeTetromino = this.tetromino[this.tetrominoN];
-  this.draw();
+
+  let nextPattern = this.tetromino[(this.tetrominoN + 1)%this.tetromino.length];
+  let kick = 0;
+
+  if (this.collision(0, 0, nextPattern)) {
+    if (this.x > COL/2) {
+      //mur droit
+      kick = -1;//pour déplacer la pièce vers la droite
+    } else {
+      //mur gauche
+      kick = 1;//pour déplacer la pièce vers la gauche
+    }
+  }
+  if (!this.collision(kick, 0, nextPattern)) {
+    this.unDraw();
+    this.x += kick;
+    this.tetrominoN = (this.tetrominoN + 1)%this.tetromino.length;
+    this.activeTetromino = this.tetromino[this.tetrominoN];
+    this.draw();
+  }
 }
+
+//je déclare le score
+let score = 0;
+
+//fonction ermettant de bloquer une pièce dès qu'elle touche le sol de quadrillage
+Piece.prototype.lock = function () {
+
+  for (r = 0; r < this.activeTetromino.length; r++) {
+    for (c = 0; c < this.activeTetromino.length; c++) {
+      //on passe à la pièce suivante
+      if (!this.activeTetromino[r][c]) {
+        continue;
+      }
+
+      //fonction qui renvoit à un game over
+      if (this.y + r < 0) {
+        alert("game over");
+        gameOver = true;
+        break;// arret de la fonction requestAnimationFrame.
+      }
+
+      board[this.y+r][this.x+c] = this.color;
+    }
+  }
+
+  //suppression des lignes pour en créer de nouvelles.
+  for (r = 0; r < ROW; r++) {
+    let isRowFull = true;
+
+    for (c = 0; c < COL; c++) {
+      isRowFull = isRowFull && (board[r][c] != VACANT);
+    }
+
+    if (isRowFull) {
+      //faire descendre les lignes pleines
+      for (y = r; y > 1; y--) {
+        for (c = 0; c < ROW; c++) {
+          board[y][c] = board[y-1][c];
+        }
+      }
+
+      for (c = 0; c < ROW; c++) {
+        board[0][c] = VACANT;
+      }
+
+      //incrémenter le score de 10
+      score += 10;
+    }
+  }
+  drawBoard();
+}
+
+//détecter une collision
+Piece.prototype.collision = function (x, y, piece) {
+  for (r = 0; r < piece.length; r++) {
+
+    for (c = 0; c < piece.length; c++) {
+      //on s'arrête si une cellule de la grill est occupée
+      if (!piece[r][c]) {
+        continue;
+      }
+
+      let newX = this.x + c + x;
+      let newY = this.y + r + y;
+
+      //la condition
+      if (newX < 0 || newX >= COL || newY >= ROW) {
+        return true;
+      }
+
+      if (newY < 0) {
+        continue;
+      }
+
+      if (board[newY][newX] != VACANT) {
+        return true;
+
+      }
+    }
+  }
+  return false;
+}
+
 
 //contrôler la pièce
 //On écoute un évènement, le fait d'appuier sur une touche.
@@ -154,6 +272,8 @@ function CONTROL(event) {
 
 //la fréquence à laquelle les pièces vont tomber
 let dropStart = Date.now();
+let gameOver = false;
+
 function drop() {
   let now = Date.now();
   let delta = now - dropStart;
@@ -163,6 +283,10 @@ function drop() {
     dropStart = Date.now();
   }
 
-  requestAnimationFrame(drop);
+  if (!gameOver) {
+    requestAnimationFrame(drop);
+  }
+
+
 }
 drop();
